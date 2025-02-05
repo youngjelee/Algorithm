681d21f13bdc3b89b77ef6c16b25814e5b127ea2
https://velog.io/@wpdud1221 <40883063+youngjelee@users.noreply.github.com>
2025-02-06 01:57:50 +0900

    [Gold V] Title: 회의실 배정, Time: 2804 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,23 +1,18 @@
-# 첫째 줄에 회의의 제공받음
-N = int (input())
+# 첫쨰줄에 미팅룸 배열받음
+N = int(input())
+# N 숫자만큼 String 받아서 공백으로 int 배열 split   [ [1,2] , [2,4] ]
+meetings = [ list(map(int , input().split()))  for _ in range(N) ]
 
-# 미팅 배열
-meetings  = [ list(map( int ,input().split()))  for _ in range(N) ]
+# meetings 를 종료시간 / 시작시간 asc 로 sorting
+meetings.sort( key=lambda x: (x[1] , x[0]) )
 
-# 미팅 sorting , 1) 종료시간 2) 시작시간
-meetings.sort(key=lambda x: [ x[1] , x[0]])
+meetingCnt = 0
+lastTime = 0
 
-
-meeting_cnt = 0
-end_time = 0
-
-# 그리디 알고리즘 적용
-for start, end in meetings:
-    if start >= end_time:  # 이전 회의가 끝난 이후에 시작하는 경우
-        meeting_cnt += 1
-        end_time = end  # 현재 회의가 끝난 시간으로 업데이트
-
-# 결과 출력
-print(meeting_cnt)
+for meeting in meetings :
+    if lastTime <= meeting[0] :
+        meetingCnt +=1
+        lastTime = meeting[1]
 
 
+print(meetingCnt)
