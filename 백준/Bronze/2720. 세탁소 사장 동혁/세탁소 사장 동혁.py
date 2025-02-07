f1f1e6e797772a470b9f1e94cb6a06478d353b94
https://velog.io/@wpdud1221 <40883063+youngjelee@users.noreply.github.com>
2025-02-08 01:20:31 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,29 @@
+
+N = int( input()) # N개 입력받음
+
+coinType = [25 , 10 , 5 , 1 ] #코인종류
+
+inputArr = [ int(input()) for i in range(N) ] # [124 ,25,194]
+
+outputArr = []
+for idx , val in enumerate(inputArr) :
+
+    tmp =[]
+
+    for i in range(len(coinType)) :
+
+        if coinType[i] <= 0 :
+            tmp.append(str(0))
+
+        if val // coinType[i] > 0:
+            tmp.append(str(val // coinType[i]))
+            val -=  coinType[i] * (val // coinType[i] )
+        else:
+            tmp.append(str(0))
+    outputArr.append( " ".join(tmp))
+
+
+for r in outputArr :
+    print(r)
+
+
