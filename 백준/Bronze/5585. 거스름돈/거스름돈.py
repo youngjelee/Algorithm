f7a9c3a2f575a88d880c0a7e503c14e3a01acbcc
https://velog.io/@wpdud1221 <40883063+youngjelee@users.noreply.github.com>
2025-02-08 01:47:07 +0900

    [Bronze II] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,14 +1,14 @@
-totalMount = 1000 - int(input())
+change = 1000 - int(input())
 coins = [500,100,50,10,5,1]
 
 minChangeCount = 0
 for coin in coins :
 
-    if totalMount <= 0 :
+    if change <= 0 :
         break
 
-    if totalMount // coin > 0 :
-        minChangeCount += (totalMount // coin )
-        totalMount -= coin * (totalMount // coin )
+    if change // coin > 0 :
+        minChangeCount += change // coin
+        change %= coin
 
 print(minChangeCount)
